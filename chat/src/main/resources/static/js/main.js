a2ec8cd083c8d44e4bc3744ca78996efaa7c68c7
Mert MURAT <58560920+MertMURAT@users.noreply.github.com>
2025-01-17 12:35:43 +0300

    Client side version 4 Chat page (JavaScript implementation)

--- chat/src/main/resources/static/js/main.js
+++ chat/src/main/resources/static/js/main.js
@@ -35,5 +35,34 @@ function onError(error){
     $('#connecting').text('Could not connect to WebSocket server. Please refresh this page to try again!').css('color', 'red');
 }
 
+function sendMessage(event)
+{
+    var messageContent = $("#message").val().trim();
+    if (messageContent && stompClient){
+        var chatMessage = {
+            sender: username,
+            content: $("#message").val(),
+            type: 'CHAT'
+        };
+        stompClient.send("/app/chat.sendMessage",
+            {},
+            JSON.stringify(chatMessage));
+        $("#message").val('');
+    }
+    event.preventDefault();
+}
+
+function onMessageReceived(payload){
+    var message = JSON.parse(payload.body);
+    if(message.type === 'JOIN'){
+        $('#message-area').prepend(`<tr><td class="text-secondary fs-6">${message.sender} joined!</td></tr>`);
+} else if (message.type === 'LEAVE'){
+        $('#message-area').prepend(`<tr><td class="text-secondary fs-6">${message.sender} left!</td></tr>`);
+    } else {
+        $('#message-area').prepend(`<tr><td class="text-secondary fs-5"><b>${message.sender}: </b> ${message.content}</td></tr>`);
+    }
+}
+
 // Add EventListener
 $("username-form").on("submit", connect);
+$("message-form").on("submit", sendMessage);
